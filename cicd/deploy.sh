b935fe23b969a85af51091d2ac6eaa53ff7bfcbf
luckyfenix <garapkor@gmail.com>
2020-09-06 15:27:37 -0700

    update ci pipeline

---
+++ cicd/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker build -t horadrik/multi-client:$GIT_HASH -f ./client/Dockerfile ./client
+docker build -t horadrik/multi-server:$GIT_HASH -f ./server/Dockerfile ./server
+docker build -t horadrik/multi-worker:$GIT_HASH -f ./worker/Dockerfile ./worker
+
+docker push horadrik/multi-client:$GIT_HASH
+docker push horadrik/multi-server:$GIT_HASH
+docker push horadrik/multi-worker:$GIT_HASH
+
+kubectl apply -f k8s
+
+kubectl set image deployments/client-deployment client=horadrik/multi-client:$GIT_HASH
+kubectl set image deployments/server-deployment server=horadrik/multi-server:$GIT_HASH
+kubectl set image deployments/worker-deployment worker=horadrik/multi-worker:$GIT_HASH
